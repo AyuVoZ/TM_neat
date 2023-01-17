b07ed8e566906aac1b1d7936107193973b1a67aa
Nwen <gwendalpaton@gmail.com>
2023-01-17 15:21:11 +0100

    config gampead

---
+++ config_gampad.py
@@ -0,0 +1,17 @@
+import vgamepad as vg
+import time
+gamepad = vg.VX360Gamepad()
+time.sleep(15)
+gamepad.left_trigger_float(value_float=1)
+gamepad.update()
+time.sleep(1)
+gamepad.reset()
+time.sleep(4)
+gamepad.right_trigger_float(value_float=1)
+gamepad.update()
+time.sleep(1)
+gamepad.reset()
+time.sleep(4)
+gamepad.left_joystick_float(x_value_float=1, y_value_float=0)
+gamepad.update()
+time.sleep(50)
