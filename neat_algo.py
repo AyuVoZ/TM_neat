f4b4e464eeeefcdda1cdf6e64b90f1f707efcb1f
Gwendal Paton <gwendalpaton@gmail.com>
2023-01-24 15:27:27 +0100

    visualize

--- neat_algo.py
+++ neat_algo.py
@@ -174,18 +174,15 @@ def run():
 
     print(winner)
 
-    # visualize.plot_stats(stats, ylog=True, view=True, filename="feedforward-fitness.svg")
-    # visualize.plot_species(stats, view=True, filename="feedforward-speciation.svg")
+    visualize.plot_stats(stats, ylog=True, view=True, filename="feedforward-fitness.svg")
+    visualize.plot_species(stats, view=True, filename="feedforward-speciation.svg")
 
-    # node_names = {-1: 'x', -2: 'dx', -3: 'theta', -4: 'dtheta', 0: 'control'}
-    # visualize.draw_net(config, winner, True, node_names=node_names)
+    node_names = {-1: 'x', -2: 'dx', -3: 'theta', -4: 'dtheta', 0: 'control'}
+    visualize.draw_net(config, winner, True, node_names=node_names)
 
-    # visualize.draw_net(config, winner, view=True, node_names=node_names,
-    #                    filename="winner-feedforward.gv")
-    # visualize.draw_net(config, winner, view=True, node_names=node_names,
-    #                    filename="winner-feedforward-enabled.gv", show_disabled=False)
-    # visualize.draw_net(config, winner, view=True, node_names=node_names,
-    #                    filename="winner-feedforward-enabled-pruned.gv", show_disabled=False, prune_unused=True)
+    visualize.draw_net(config, winner, view=True, node_names=node_names,filename="winner-feedforward.gv")
+    visualize.draw_net(config, winner, view=True, node_names=node_names,filename="winner-feedforward-enabled.gv", show_disabled=False)
+    visualize.draw_net(config, winner, view=True, node_names=node_names,filename="winner-feedforward-enabled-pruned.gv", show_disabled=False, prune_unused=True)
 
 
 if __name__ == '__main__':
